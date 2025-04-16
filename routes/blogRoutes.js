a095d84779cda88d36e0f2e10b9cd1e5c973b0ba
hemanth622 <hemuh5652@gmail.com>
2025-04-17 00:48:35 +0530

    Add files via upload

---
+++ routes/blogRoutes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const {
+  getAllBlogsController,
+  createBlogController,
+  updateBlogController,
+  getBlogByIdController,
+  deleteBlogController,
+  userBlogController,
+} = require("../controllers/blogController");
+
+const router = express.Router();
+
+router.get("/all-blog", getAllBlogsController);
+
+// CREATE USER || POST
+router.post("/create-blog", createBlogController);
+
+//LOGIN || POST
+router.put("/update-blog/:id", updateBlogController);
+
+router.get("/get-blog/:id", getBlogByIdController);
+
+router.delete("/delete-blog/:id", deleteBlogController);
+
+router.get("/user-blog/:id", userBlogController);
+
+
+module.exports = router;
---
+++ routes/userRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const {
+  getAllUsers,
+  registerController,
+  loginController,
+} = require("../controllers/userController");
+
+
+const router = express.Router();
+
+router.get("/all-users", getAllUsers);
+
+// CREATE USER || POST
+router.post("/register", registerController);
+
+//LOGIN || POST
+router.post("/login", loginController);
+
+module.exports = router;
